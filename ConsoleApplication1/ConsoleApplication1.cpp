4f46a7d6e7f3d705634297c3cf6ad2c78175ceac
Bing-Yi Wong <antares_true@hotmail.com>
2013-05-22 23:08:03 +0800

    A simple test of vc project

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,13 @@
+// ConsoleApplication1.cpp : 定義主控台應用程式的進入點。
+//
+
+#include "stdafx.h"
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout << "Hello World" << endl;
+	cin.get();
+	return 0;
+}
+
